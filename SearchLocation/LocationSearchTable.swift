27f5e8544d5f8572200a1f33104b2a65fc783c67
Antonina <a.lehka@mobidev.biz>
2020-04-27 16:42:03 +0300

    added search controller

---
+++ SearchLocation/LocationSearchTable.swift
@@ -0,0 +1,25 @@
+//
+//  LocationSearchTable.swift
+//  SearchLocation
+//
+//  Created by User on 27.04.2020.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationSearchTable : UITableViewController{
+    
+    
+    
+    
+    
+    
+    
+}
+extension LocationSearchTable : UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
--- SearchLocation/ViewController.swift
+++ SearchLocation/ViewController.swift
@@ -7,14 +7,127 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
+    var showMapRoute = false
+    var stepCounter = 0
+    let locationDistance : Double = 500
+    var steps : [MKRoute.Step] = []
+    // var route : MKRoute
+    var  resultSearchController : UISearchController? = nil
+    
+  
+    @IBOutlet weak var mapView: MKMapView!
+    lazy var locationManager: CLLocationManager = {
+        let locationManger = CLLocationManager()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            
+            print("Location is enable->")
+            locationManger.delegate = self
+            locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        }
+        else {
+            print("Location services are not enabled")
+            
+        }
+         return locationManger
+    }()
+    
+    fileprivate func handleAuthorizationStatus(locationManager: CLLocationManager, status: CLAuthorizationStatus){
+        switch status {
+       
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            // view current location if we have coordinates
+            if let center = locationManager.location?.coordinate{
+                centerViewToUserLocation(center: center)
+            }
+            break
+        @unknown default:
+            break
+        }
+        
+        
+    }
+    fileprivate func centerViewToUserLocation(center: CLLocationCoordinate2D){
+        // set current location
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: locationDistance, longitudinalMeters: locationDistance)
+        self.mapView.setRegion(region, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // whant see user location
+            mapView.delegate = self
+            mapView.mapType = .standard
+            mapView.showsCompass = true
+            mapView.showsScale = true
+            mapView.showsUserLocation = true
+        
+        let locationSearchTable = storyboard!.instantiateViewController(identifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable as UISearchResultsUpdating
+            
+        //setup search bar
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        // navigation Bar disappears when the search results are shown if false-> searchBar accessible all time
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        // gives the modal overlay a semi-transparent background when the search bar is selected
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        
+        // start get/update location
+        locationManager.startUpdatingLocation()
     }
 
+    }
+fileprivate func getMapRoute(){
+   
+}
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // updated user location if if showMapRoute = false
+        if !showMapRoute {
+            if let location = locations.last {
+                let center  = location.coordinate
+                centerViewToUserLocation(center: center)
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // handle if change authorization status
+        handleAuthorizationStatus(locationManager: locationManager, status: status)
+    }
+    
+     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        stepCounter += 1
+        
+      
+        
+        
+    }
+    
+    
+}
+extension ViewController: MKMapViewDelegate{
+    
+    
 }
 
