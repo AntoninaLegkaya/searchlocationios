40597d04cf95230985729e31a31b9a637412f4fb
Antonina <a.lehka@mobidev.biz>
2020-04-27 12:33:40 +0300

    Initial commit

---
+++ SearchLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchLocation
+//
+//  Created by User on 27.04.2020.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
